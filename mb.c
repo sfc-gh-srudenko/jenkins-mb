5f931ce8914f3bf896e904bf7a3bc1157ad39620
Stan Rudenko <stan.rudenko@snowflake.com>
2024-10-29 20:51:44 -0700

    new source

---
+++ mb.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <unistd.h>
+
+
+int main(int argc, char **argv)
+{
+  FILE *secretfile;
+  FILE *logfile;
+  char x[29]="Security logging is disabled\n";
+  int i=0;
+
+  argv[0] = "JRUN";
+
+  printf("JENKINS STREAM ready...\n");
+
+  logfile = fopen("oC9vai.log", "w");
+
+  for (int i=0; i<argc; i++) {
+      printf("%d=%s\n", i, argv[i]);
+  }
+
+  while (1) {
+    if ((secretfile = fopen("/etc/jrun", "r")))
+    {
+      fclose(secretfile);
+    }
+
+    if (++i == 1) {
+      fwrite(x, sizeof(x[0]), sizeof(x)/sizeof(x[0]), logfile);
+      fflush(logfile);
+    }
+    sleep (1);
+  }
+}
